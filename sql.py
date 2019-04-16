9fdb5702000936c6c06c33684db1bbcfb41934e3
mrintro <40711272+mrintro@users.noreply.github.com>
2019-04-16 23:30:44 +0530

    Delete sql.py

--- sql.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(host="localhost",user="root",database = "josaa")
-mycursor = mydb.cursor()
-mycursor.execute("select * from college")
-res = mycursor.fetchall()
-for i in res:
-	print(i)
